57944e16da28a1676a67bf060bdb656fcaceabf8
Jaymi02 <Jaymi02@users.noreply.github.com>
2024-03-26 17:08:08 +0200

    My first website.

---
+++ main.js
@@ -0,0 +1,7 @@
+const dark = "#2e2e2e";
+const light = "#ededed";
+
+document.documentElement.style.setProperty("--background", dark);
+document.documentElement.style.setProperty("--foreground", light);
+
+
